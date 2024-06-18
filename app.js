715a219db722d1e3a5ac41c681fc8628473e8865
clam <145952290+aegisen@users.noreply.github.com>
2024-06-18 23:28:02 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+new DataTable('#example', {
+    initComplete: function () {
+        this.api()
+            .columns()
+            .every(function (index) {
+                if (index != 1)
+                    {
+                        let column = this;
+        
+                        // Create select element
+                        let select = document.createElement('select');
+                        select.add(new Option(''));
+                        column.footer().replaceChildren(select);
+        
+                        // Apply listener for user change in value
+                        select.addEventListener('change', function () {
+                            column
+                                .search(select.value, {exact: true})
+                                .draw();
+                        });
+        
+                        // Add list of options
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.add(new Option(d));
+                            });
+                    }
+                
+            });
+    }
+});
